9ec4376a26aedfcca50fb4c3bad15cb2f4f7f87a
Cory Parsnipson <cparsnipson@gmail.com>
2023-08-17 21:46:37 -0700

    Incorporate interactive stdin and add async response.

--- internet-tokio/src/main.rs
+++ internet-tokio/src/main.rs
@@ -1,15 +1,67 @@
 use rand::Rng;
+use std::cmp::max;
+use std::io::{self, Write};
+use std::thread;
 use tokio::io::Result;
+use tokio::runtime::Handle;
 use tokio::time::{sleep, Duration};
 
 const MAX_DELAY: i64 = 3000; // in milliseconds
+const MIN_RESPONDERS: usize = 1;
+const MAX_RESPONDERS: usize = 10;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut handles = vec![];
+    let tokio_h = Handle::current();
 
-    for tid in 0..10 {
-        handles.push(tokio::spawn(async move {
+    let jh = thread::spawn(move || {
+        println!("Starting internet simulator...");
+
+        loop {
+            print!("Name a thing: ");
+            io::stdout().flush().unwrap();
+
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("failed to read line");
+
+            let input = String::from(input.trim());
+            let handle = tokio_h.clone();
+
+            let th = tokio_h.spawn(async move {
+                let num_responders = {
+                    let mut rng = rand::thread_rng();
+                    let num = rng.gen::<usize>() % MAX_RESPONDERS;
+
+                    max(MIN_RESPONDERS, num)
+                };
+
+                response(&handle, input.as_str(), num_responders).await;
+            });
+
+            // not sure if this is the right way, but we need to wait for the
+            // tokio threads to finish. We can't use await because this closure
+            // from spawned thread is not async.
+            while !th.is_finished() {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+            }
+        }
+    });
+    jh.join().expect("input thread has panicked");
+
+    println!("Hello, world!");
+    Ok(())
+}
+
+async fn response(handle: &Handle, topic: &str, num_responders: usize) {
+    let mut handles = Vec::new();
+    handles.reserve(num_responders);
+
+    for _ in 0..num_responders {
+        let topic = String::from(topic);
+
+        handles.push(handle.spawn(async move {
             let delay_amount = {
                 // calculate delay this way because I want them to be grouped in clusters
                 let mut rng = rand::thread_rng();
@@ -20,17 +72,15 @@ async fn main() -> Result<()> {
                 (if delay > 0 { delay } else { 0 }) as u64
             };
 
-            println!("Thread {tid}: starting...");
             sleep(Duration::from_millis(delay_amount)).await;
-            println!("Thread {tid}: delayed for {delay_amount}ms!!");
+            println!("{} SUCKS!!", topic);
         }));
     }
 
-    // keep main thread alive until the spawned threads execute
     for handle in handles {
         let _ = handle.await.unwrap();
     }
 
-    println!("Hello, world!");
-    Ok(())
+    // put in whitespace at the very end
+    println!("");
 }
